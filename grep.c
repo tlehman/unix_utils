ff611d12ea9857d7926428eeaf0ca56d083b4732
Tobi Lehman <tobi.lehman@gmail.com>
2013-11-17 22:55:15 -0800

    fill out grep-skeleton; add un-license

--- grep.c
+++ grep.c
@@ -1,15 +1,43 @@
+#include <stdio.h>
+#include <regex.h>
+
+int match(const char *regexp, const char *line);
+int grep(const char *regexp, FILE *f);
+
+
 int main(int argc, const char *argv[])
 {
     if(2 == argc) {
-        // grep over stdin, since this point is reached by:
-        // <command> | ./grep 'pat*ern'
+        // grep stdin
     } else {
-        /* loop over the remaining (argc - 2) args, 
-           open each as file,
-             loop over each line of file,
-               check if line matches pattern, if so, print line
-           */
+        // loop over remaining argc - 2 files and grep each
+        for(int i = 2; i < argc; ++i) {
+            // open file
+            // grep file
+            // close file
+        }
     }
 
     return 0;
 }
+
+int grep(const char *regexp, FILE *f)
+{
+    char buf[BUFSIZ];
+    int nmatches;
+
+    while( NULL != fgets(buf, BUFSIZ, f) ) {
+        if(match(regexp, buf)) {
+            printf("%s\n", buf);
+            ++nmatches;
+        }
+    }
+
+    return nmatches;
+}
+
+int match(const char *regexp, const char *line)
+{
+    return 0;
+}
+
