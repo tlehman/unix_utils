6e404495657b09700429fd6377d20b169b1501e4
Tobi Lehman <tobi.lehman@gmail.com>
2013-11-19 21:01:35 -0800

    add to wc.c

--- wc.c
+++ wc.c
@@ -2,14 +2,35 @@
     by tlehman at 1384790932 */
 #include <stdio.h>
 
+#define is_whitespace
+
 int main(int argc, const char *argv[])
 {
+    char c;
+    int in_word = 0;
+    int chars, words, lines = 0, 0, 0;
+
     if(1 == argc) {
-        printf("%d  %d  %d", 
-                count_lines(),
-                count_words(),
-                count_chars());
+        // read from pipe
+        do {
+            c = getc(stdin);
+
+            ++chars;
+            if('\n' == c) ++lines;
+
+            if(in_word && ' ' == c)
+
+        } while(c != EOL);
+    } else {
+        // argv[1] is filename
+        // open file
+        // read character
+        // if EOL
+        //   close file
     }
+
+    printf("%d  %d  %d", lines, words, chars);
+
     return 0;
 }
 
