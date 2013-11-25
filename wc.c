a6326237199e1248f715bad180ddb1277b0c9a83
Tobi Lehman <tobi.lehman@gmail.com>
2013-11-24 22:10:41 -0800

    spit out filename after processing file

--- wc.c
+++ wc.c
@@ -2,31 +2,32 @@
     by tlehman at 1384790932 */
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
-void wc(FILE *file);
+void wc(const char *filename);
 
 int main(int argc, const char *argv[])
 {
     if(1 == argc) {
         // read from pipe
-        wc(stdin);
+        wc("/dev/stdin");
     } else {
         for(int i = 1; i < argc; ++i) {
-            FILE *file = fopen(argv[i], "r");
-            wc(file);
-            fclose(file);
+            wc(argv[i]);
         }
     }
 
     return 0;
 }
 
-void wc(FILE *file)
+void wc(const char *filename)
 {
     char c;
     int in_word = 0;
     int chars, words, lines = 0;
 
+    FILE *file = fopen(filename, "r");
     do {
         c = getc(file);
 
@@ -42,8 +43,16 @@ void wc(FILE *file)
         in_word = !isspace(c);
 
     } while(c != EOF);
+    fclose(file);
 
-    printf("%d  %d  %d\n", lines, words, chars);
+    // indent width = length(chars num) - length(lines num)
+    int width = (int)(ceil(log10(chars)) - ceil(log10(lines)));
+
+    if(strcmp("/dev/stdin", filename) == 0) {
+        printf("%2d  %d  %d\n", lines, words, chars);
+    } else {
+        printf("%2d  %d  %d  %s\n", lines, words, chars, filename);
+    }
 }
 
 
